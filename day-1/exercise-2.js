b63f9750c62ef552c1003eb8b119f3795fae2bec
votat merlin <votatm@gmail.com>
2022-04-20 14:45:48 +0200

    fix exo 2

--- day-1/exercise-2.js
+++ day-1/exercise-2.js
@@ -1,1 +1,8 @@
-export const my_display_alpha_reverse = () => "zyxwvutsrqponmlkjihgfedcba";
+import { my_display_alpha } from "../index.js";
+export const my_display_alpha_reverse = () => {
+  let result = "";
+  for (let i = 25; i >= 0; i--) {
+    result += my_display_alpha()[i];
+  }
+  return result;
+};
